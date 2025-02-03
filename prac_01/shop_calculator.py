f27f009bf25844348bb969406a12baa6bce0f508
NhatThienNguyen <nhatthien.nguyen@my.jcu.edu.au>
2025-02-03 18:19:06 +0800

    write program to transfer celsius to farenheit or fahrenheit to celsius

---
+++ prac_01/shop_calculator.py
@@ -0,0 +1,18 @@
+DISCOUNT_THRESHOLD = 100
+DISCOUNT_RATE = 0.1
+
+number_of_items = int(input("Number of items: "))
+total_price = 0
+
+while number_of_items <= 0:
+    print("Invalid number of items")
+    number_of_items = int(input("Number of items: "))
+
+for i in range(number_of_items):
+    price_of_items = float(input("Price of items: "))
+    total_price += price_of_items
+
+if total_price > DISCOUNT_THRESHOLD:
+    total_price = total_price - (total_price * DISCOUNT_RATE)
+
+print(f"Total price for {number_of_items} items is ${total_price:.2f}")
