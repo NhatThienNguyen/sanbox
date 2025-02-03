78e1cef35456ddfab1a3ad228230bbb8c2475be4
NhatThienNguyen <nhatthien.nguyen@my.jcu.edu.au>
2025-02-03 18:30:19 +0800

    add password check program

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -1,5 +1,14 @@
-password = input("Enter your password: ")
-while len(password) <= 0:
-    print("Invalid password")
+def main():
+    password = get_password()
+    print(len(password)* "*")
+
+
+def get_password():
     password = input("Enter your password: ")
-print(len(password)* "*")
+    while len(password) <= 0:
+        print("Invalid password")
+        password = input("Enter your password: ")
+    return password
+
+
+main()
