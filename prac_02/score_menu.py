89ef04b4c4c146f0979ca75d15776e846da788b5
NhatThienNguyen <nhatthien.nguyen@my.jcu.edu.au>
2025-02-04 09:43:14 +0800

    add validate score function

--- prac_02/score_menu.py
+++ prac_02/score_menu.py
@@ -1,11 +1,14 @@
 MENU = "(G)et valid score\n(P)rint result\n(S)how stars\n(Q)uit"
 
 def main():
+    score = int(input("Enter your score: "))
+    valid_score = validate_score(score)
     print(MENU)
     choice = input(">>> ").upper()
     while choice != "Q":
         if choice == "G":
-            pass
+            score = int(input("Enter your score: "))
+            valid_score = validate_score(score)
         elif choice == "P":
             pass
         elif choice == "S":
@@ -15,6 +18,13 @@ def main():
         print(MENU)
         choice = input(">>> ").upper()
     print("farewell")
+
+
+def validate_score(score):
+    while score < 0 or score > 100:
+        print("Invalid score")
+        score = int(input("Enter your score: "))
+    return score
 
 
 main()
