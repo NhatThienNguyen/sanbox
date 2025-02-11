11954520ad44cf35f2c07bb96c7d8a4a1195bc6f
NhatThienNguyen <nhatthien.nguyen@my.jcu.edu.au>
2025-02-11 09:31:58 +0800

    finished code

---
+++ prac_03/randoms.py
@@ -0,0 +1,12 @@
+import random
+
+print(random.randint(5, 20))  # line 1
+print(random.randrange(3, 10, 2))  # line 2
+print(random.uniform(2.5, 5.5))  # line 3
+
+"""
+line 1: The smallest was 5, biggest was 18
+line 2:the smallest was 3, biggest was 5
+line 3: the smallest was 3.1427, biggest was 3,9954
+"""
+print(random.randint(1,100))
