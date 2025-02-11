7f1019e26129a0c977d814863f5574aed2fd16d5
NhatThienNguyen <nhatthien.nguyen@my.jcu.edu.au>
2025-02-11 10:20:19 +0800

    finish read name in other file

--- prac_03/files.py
+++ prac_03/files.py
@@ -1,4 +1,10 @@
+# 1.
 out_file = open("name.txt","w")
 name = input("Enter your name: ")
 print(name, file=out_file)
 out_file.close()
+# 2.
+in_file = open("name.txt","r")
+input_name = in_file.read().strip()
+in_file.close()
+print(f"Hi {input_name}!")
