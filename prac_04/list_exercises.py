7016b7f1ac4cefa08e205a0a74bea772976040cf
NhatThienNguyen <nhatthien.nguyen@my.jcu.edu.au>
2025-02-18 09:49:33 +0800

    finish first task

--- prac_04/list_exercises.py
+++ prac_04/list_exercises.py
@@ -1,4 +1,6 @@
 numbers = []
+usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45', 'BaseInterpreterInterface', 'BaseStdIn',
+             'Command', 'ExecState', 'InteractiveConsole', 'InterpreterInterface', 'StartServer', 'bob']
 
 for i in range(5):
     number = float(input('Number: '))
@@ -9,3 +11,9 @@ print(f"The last number is {numbers[-1]}")
 print(f"The smallest number is {min(numbers)}")
 print(f"The largest number is {max(numbers)}")
 print(f"The average number is {sum(numbers) / len(numbers)}")
+
+name = input("What's your username? ")
+while name not in usernames:
+    print("Access Denied")
+    name = input("What's your username? ")
+print("Access granted")
