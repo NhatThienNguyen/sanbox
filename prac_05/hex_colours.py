bca1166454081639d083a4267ef625a70ae0277b
NhatThienNguyen <nhatthien.nguyen@my.jcu.edu.au>
2025-02-25 10:19:52 +0800

    add dictionary

---
+++ prac_05/hex_colours.py
@@ -0,0 +1,4 @@
+CODE_TO_COLOUR = {"Absolute Zero": "#0048ba", "Acid Green": "#b0bf1a", "AliceBlue": "#f0f8ff",
+                  "Alizarin Crimson": "#e32636", "Amaranth": "#e52b50", "Amber": "#ffbf00", "Amethyst": "#9966cc",
+                  "Apricot": "#fbceb1", "Aqua": "#00ffff", "Army Green":"#4b5320"}
+
