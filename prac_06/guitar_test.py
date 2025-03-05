a1592b991934937d61d8fc700d17b9f9afcfe186
NhatThienNguyen <nhatthien.nguyen@my.jcu.edu.au>
2025-03-05 12:09:47 +0800

    add two guitars and check if the class work

---
+++ prac_06/guitar_test.py
@@ -0,0 +1,12 @@
+"""
+Estimate time: 20 minutes
+Actual time:
+"""
+from prac_06.guitar import Guitar
+
+guitar = Guitar(name="Gibson L-5 CES", year=1922, cost=16035.40)
+another_guitar = Guitar(name="Gibson L-5 CES", year=2013, cost=16035.40)
+print(guitar.get_age()) #Expect 103. Got 103
+print(another_guitar.get_age()) #expect 12. Got 12
+print(guitar.is_vintage()) #Expect True. Got True
+print(another_guitar.is_vintage()) #Expect False. Got False
