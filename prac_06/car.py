dad98a014b6dae023b2587857c24060808b356b3
NhatThienNguyen <nhatthien.nguyen@my.jcu.edu.au>
2025-03-05 11:20:54 +0800

    add name field on car.py, modify code on used_car and test

--- prac_06/car.py
+++ prac_06/car.py
@@ -4,13 +4,14 @@
 class Car:
     """Represent a Car object."""
 
-    def __init__(self, fuel=0):
+    def __init__(self, fuel=0, name=''):
         """Initialise a Car instance.
 
         fuel: float, one unit of fuel drives one kilometre
         """
         self.fuel = fuel
         self._odometer = 0
+        self.name = name
 
     def add_fuel(self, amount):
         """Add amount to the car's fuel."""
@@ -32,4 +33,4 @@ class Car:
 
 
     def __str__(self):
-        return f"Car, fuel={self.fuel}, odometer={self._odometer}"
+        return f"Car={self.name}, fuel={self.fuel}, odometer={self._odometer}"
--- prac_06/used_car.py
+++ prac_06/used_car.py
@@ -13,11 +13,11 @@ def main():
     my_car.drive(30)
     print(f"Car has fuel: {my_car.fuel}")
     print(my_car)
-    limo = Car(fuel=100)
+    limo = Car(fuel=100,name='limo')
     limo.add_fuel(20)
-    print(limo.fuel)
+    print(limo)
     limo.drive(115)
-    print(limo.fuel)
+    print(limo)
 
 
 main()
