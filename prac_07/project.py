36687c494fabcd145342d64fa36639db2c52681c
NhatThienNguyen <nhatthien.nguyen@my.jcu.edu.au>
2025-03-18 10:11:13 +0800

    make get data function to work

--- prac_07/project.py
+++ prac_07/project.py
@@ -3,9 +3,15 @@ Estimate time: 1 hour
 Actual time:
 """
 class Project:
-    def __init__(self, name, date, priority, cost, percentage):
+    def __init__(self, name, date, priority, cost:float, percentage):
         self.name = name
         self.date = date
         self.priority = priority
         self.cost = cost
         self.percentage = percentage
+
+
+    def __str__(self):
+        return f"{self.name}, start: {self.date}, priority: {self.priority}, estimate: ${self.cost:.2f}, completion: {self.percentage}"
+
+
