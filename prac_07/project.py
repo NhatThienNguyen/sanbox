2e6c9e1a24ffb1036ebfb99232c016638076b7a4
NhatThienNguyen <nhatthien.nguyen@my.jcu.edu.au>
2025-03-18 10:53:52 +0800

    make some changes to method

--- prac_07/project.py
+++ prac_07/project.py
@@ -3,15 +3,15 @@ Estimate time: 1 hour
 Actual time:
 """
 class Project:
-    def __init__(self, name, date, priority, cost:float, percentage):
+    def __init__(self, name, date, priority:int, cost:float, complete:int):
         self.name = name
         self.date = date
         self.priority = priority
         self.cost = cost
-        self.percentage = percentage
+        self.complete = complete
 
 
     def __str__(self):
-        return f"{self.name}, start: {self.date}, priority: {self.priority}, estimate: ${self.cost:.2f}, completion: {self.percentage}"
+        return f"{self.name}, start: {self.date}, priority: {self.priority}, estimate: ${self.cost:.2f}, completion: {self.complete}%"
 
 
