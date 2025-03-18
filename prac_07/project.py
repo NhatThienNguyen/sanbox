203f2913fe51cb9bf04ca9c544211e2e073e0044
NhatThienNguyen <nhatthien.nguyen@my.jcu.edu.au>
2025-03-18 09:38:51 +0800

    create new class

---
+++ prac_07/project.py
@@ -0,0 +1,11 @@
+"""
+Estimate time: 1 hour
+Actual time:
+"""
+class Project:
+    def __init__(self, name, date, priority, cost, percentage):
+        self.name = name
+        self.date = date
+        self.priority = priority
+        self.cost = cost
+        self.percentage = percentage
