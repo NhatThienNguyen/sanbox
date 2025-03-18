f2ee890ffcce9423523dc5efbbac64624d0d54f7
NhatThienNguyen <nhatthien.nguyen@my.jcu.edu.au>
2025-03-18 10:10:47 +0800

    write str method

--- prac_07/project_management.py
+++ prac_07/project_management.py
@@ -7,8 +7,12 @@ import datetime
 
 MENU =("- (L)oad projects\n- (S)ave projects\n- (D)isplay projects\n- (F)ilter projects by date\n- (A)dd new project\n"
        "- (U)pdate projects\n- (Q)uit")
+FILE_NAME ="projects.txt"
+
 
 def main():
+    projects = get_data(FILE_NAME)
+    print(projects)
     print(f"Welcome to Pythonic Project Management\nLoaded 5 projects from projects.txt\n{MENU}")
     choice = input(">>> ").upper()
     while choice != "Q":
@@ -26,6 +30,17 @@ def main():
             pass
         print(MENU)
         choice = input(">>> ").upper()
+
+
+def get_data(file_name):
+    projects =[]
+    with open(file_name, "r") as in_file:
+        data = in_file.readlines()
+        for line in data:
+            line = line.strip().replace("\n", "").replace("\t", ", ")
+            projects.append(line)
+        projects.remove(projects[0])
+    return projects
 
 
 main()
