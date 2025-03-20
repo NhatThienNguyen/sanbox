99309435f6b46371eafdf3e87c217081fdf2f9ed
NhatThienNguyen <nhatthien.nguyen@my.jcu.edu.au>
2025-03-20 11:53:53 +0800

    add comment

--- prac_07/project.py
+++ prac_07/project.py
@@ -1,8 +1,9 @@
 """
 Estimate time: 1 hour
-Actual time:
+Actual time: 2 days and 3 hours
 """
 class Project:
+    """Project class"""
     def __init__(self, name, date, priority:int, cost:float, complete:int):
         self.name = name
         self.date = date
@@ -12,8 +13,10 @@ class Project:
 
 
     def __str__(self):
+        """Default print"""
         return f"{self.name}, start: {self.date}, priority: {self.priority}, estimate: ${self.cost:.2f}, completion: {self.complete}%"
 
 
     def __lt__(self, other):
+        """Comparing lower than"""
         return self.priority < other.priority
