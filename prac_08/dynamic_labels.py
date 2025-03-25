97ea2bde77ca4a449fa945cfe7737d3b8152db8b
NhatThienNguyen <nhatthien.nguyen@my.jcu.edu.au>
2025-03-25 10:41:37 +0800

    add comment to it

--- prac_08/dynamic_labels.py
+++ prac_08/dynamic_labels.py
@@ -3,12 +3,14 @@ from kivy.app import App
 from kivy.lang import Builder
 
 class DynamicLabelsApp(App):
+    """An app make dynamic labels for name"""
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self.names = ["Joe", "Amy", "Dave"]
 
 
     def build(self):
+        """Build kv app from kv file"""
         self.title = "Dynamic Labels"
         self.root = Builder.load_file("dynamic_labels.kv")
         self.create_labels()
@@ -16,6 +18,7 @@ class DynamicLabelsApp(App):
 
 
     def create_labels(self):
+        """Create labels based on name list"""
         for name in self.names:
             name_label = Label(text=name)
             self.root.ids.main.add_widget(name_label)
