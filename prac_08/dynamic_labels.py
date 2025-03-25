088f0566453ffba1625fc73db53e94df52a0b80b
NhatThienNguyen <nhatthien.nguyen@my.jcu.edu.au>
2025-03-25 10:39:05 +0800

    fix it so that it can run

--- prac_08/dynamic_labels.py
+++ prac_08/dynamic_labels.py
@@ -1,10 +1,8 @@
 from kivy.uix.label import Label
 from kivy.app import App
 from kivy.lang import Builder
-from kivy.properties import StringProperty
 
 class DynamicLabelsApp(App):
-    text = StringProperty()
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self.names = ["Joe", "Amy", "Dave"]
@@ -12,10 +10,15 @@ class DynamicLabelsApp(App):
 
     def build(self):
         self.title = "Dynamic Labels"
-        self.root = "dynamic_labels.kv"
+        self.root = Builder.load_file("dynamic_labels.kv")
+        self.create_labels()
         return self.root
 
 
     def create_labels(self):
         for name in self.names:
             name_label = Label(text=name)
+            self.root.ids.main.add_widget(name_label)
+
+
+DynamicLabelsApp().run()
