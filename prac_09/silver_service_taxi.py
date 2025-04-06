5d1cc7813f2b4ed4bb49c6bef127406efd1352c0
NhatThienNguyen <nhatthien.nguyen@my.jcu.edu.au>
2025-04-06 10:34:12 +0800

    fix the calculate fare method and add comment

--- prac_09/silver_service_taxi.py
+++ prac_09/silver_service_taxi.py
@@ -1,6 +1,7 @@
 from prac_09.taxi import Taxi
 
 class SilverServiceTaxi(Taxi):
+    """Represent a silver service taxi"""
     flagfall = 4.50
     def __init__(self, name, fuel, fanciness):
         super().__init__(name, fuel)
@@ -9,8 +10,10 @@ class SilverServiceTaxi(Taxi):
 
 
     def __str__(self):
+        """Return a string inherit from parent class plus from this class"""
         return f"{super().__str__()}, plus flagfall of {self.flagfall:.2f}"
 
 
-    def calculate_fare(self):
-        return self.price_per_km * self.fanciness + self.flagfall
+    def calculate_fare(self, distance):
+        """Calculate the fare of a service based on price, fanciness, distance and flagfall"""
+        return (self.price_per_km * self.fanciness) * super().drive(distance) + self.flagfall
