89d6dca5576c375a1c8daf61f13957903f971c14
NhatThienNguyen <nhatthien.nguyen@my.jcu.edu.au>
2025-04-06 12:32:02 +0800

    finish the task

--- prac_09/taxi_simulator.py
+++ prac_09/taxi_simulator.py
@@ -4,13 +4,14 @@ from prac_09.silver_service_taxi import SilverServiceTaxi
 MENU = "q)uit, c)hoose taxi, d)rive"
 
 def main():
+    """Main function with menu and all the option"""
     taxis = [Taxi("Prius", 100),
              SilverServiceTaxi("Limo", 100, 2),
              SilverServiceTaxi("Hummer", 200, 4)]
     print(f"Let's drive!\n{MENU}")
     choice = input(">>> ").lower()
     current_taxi = None
-    total_bill = 0
+    total_bill = 0.0
     while choice != 'q':
         if choice == 'c':
             i = 0
@@ -26,11 +27,23 @@ def main():
             except ValueError:
                 print("Invalid input!")
         elif choice =='d':
-            pass
+            if current_taxi is None:
+                print("You have to choose a taxi before you can drive")
+            else:
+                distance = float(input("Drive how far? "))
+                current_taxi.start_fare()
+                current_taxi.drive(distance)
+                if isinstance(current_taxi, SilverServiceTaxi):
+                    trip_cost = current_taxi.calculate_fare()
+                else:
+                    trip_cost = current_taxi.get_fare()
+                print(f"Your {current_taxi.name} trip cost you: ${trip_cost:.2f}")
+                total_bill += trip_cost
         else:
             print("Invalid option!")
-        print(f"Bill to date: {total_bill}")
+        print(f"Bill to date: ${total_bill}")
         print(MENU)
+        choice = input(">>> ").lower()
     print(f"Total trip cost: {total_bill}")
     print("Taxis are now:")
     i = 0
