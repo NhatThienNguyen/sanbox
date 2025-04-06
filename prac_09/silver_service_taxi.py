6cdb60ae90ddc2525a64574f5f0b7745f0935daf
NhatThienNguyen <nhatthien.nguyen@my.jcu.edu.au>
2025-04-06 10:23:30 +0800

    add method

--- prac_09/silver_service_taxi.py
+++ prac_09/silver_service_taxi.py
@@ -1,6 +1,7 @@
 from prac_09.taxi import Taxi
 
 class SilverServiceTaxi(Taxi):
+    flagfall = 4.50
     def __init__(self, name, fuel, fanciness):
         super().__init__(name, fuel)
         self.fanciness = float(fanciness)
@@ -8,4 +9,8 @@ class SilverServiceTaxi(Taxi):
 
 
     def __str__(self):
-        return f"{super().__str__()}, fanciness: {self.fanciness}"
+        return f"{super().__str__()}, plus flagfall of {self.flagfall:.2f}"
+
+
+    def calculate_fare(self):
+        return self.price_per_km * self.fanciness + self.flagfall
