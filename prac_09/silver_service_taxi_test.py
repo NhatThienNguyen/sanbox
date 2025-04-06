170fc433c549afdffa6c6d00d94d13e68559f37f
NhatThienNguyen <nhatthien.nguyen@my.jcu.edu.au>
2025-04-06 10:32:26 +0800

    fix the code to make sure it working

--- prac_09/silver_service_taxi_test.py
+++ prac_09/silver_service_taxi_test.py
@@ -1,5 +1,5 @@
 from prac_09.silver_service_taxi import SilverServiceTaxi
 
-my_taxi = SilverServiceTaxi(name='Tesla', fuel=100, fanciness=5)
+my_taxi = SilverServiceTaxi(name='Tesla', fuel=100, fanciness=2)
 print(my_taxi)
-print(my_taxi.calculate_fare())
+print(my_taxi.calculate_fare(18))
