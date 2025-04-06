0880e8d6afba8e8dad10d4858268cb8ae75f4565
NhatThienNguyen <nhatthien.nguyen@my.jcu.edu.au>
2025-04-06 11:29:21 +0800

    add comment

--- prac_09/band.py
+++ prac_09/band.py
@@ -1,18 +1,23 @@
 class Band:
+    """Class band"""
     def __init__(self, name=''):
+        """Represent band"""
         self.name = name
         self.musicians = []
 
 
     def __str__(self):
+        """Return string"""
         return f"{self.name} ({', '.join(str(musician) for musician in self.musicians)})"
 
 
     def add(self,musician):
+        """Add musician to musicians list"""
         return self.musicians.append(musician)
 
 
     def play(self):
+        """return string showing band playing"""
         result = []
         for musician in self.musicians:
             result.append(musician.play())
