fc326d7f1bc1b966154d61e69b779678e30a3a18
NhatThienNguyen <nhatthien.nguyen@my.jcu.edu.au>
2025-04-08 12:16:38 +0800

    complete the while loop and handle error

--- prac_10/wiki.py
+++ prac_10/wiki.py
@@ -1,6 +1,6 @@
 import wikipedia
 
-page_title = input("Enter page title: ").strip()
+page_title = input("Enter page title: ")
 
 while page_title !='':
     try:
@@ -8,4 +8,10 @@ while page_title !='':
         print(f"{page_title}")
         print(page.summary)
         print(page.url)
-    except wikipedia.exceptions.DisambiguationError:
+    except wikipedia.exceptions.DisambiguationError as e:
+        print(f"We need a more specific title. Try one of the following, or a new search: ")
+        print(e.options)
+    except wikipedia.exceptions.PageError:
+        print(f"Page id {page_title} does not match any pages. Try another id!")
+    page_title = input("Enter page title: ")
+print("Thank you")
