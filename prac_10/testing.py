325908cce05b89b0b377042e44abbfd64a3c49cf
NhatThienNguyen <nhatthien.nguyen@my.jcu.edu.au>
2025-04-08 11:40:47 +0800

    fix is long word function

--- prac_10/testing.py
+++ prac_10/testing.py
@@ -22,7 +22,7 @@ def is_long_word(word, length=5):
     >>> is_long_word("Python", 6)
     True
     """
-    return len(word) > length
+    return len(word) >= length
 
 
 def run_tests():
@@ -38,9 +38,10 @@ def run_tests():
     # assert test with custom message,
     # used to see if Car's init method sets the odometer correctly
     # this should pass (no output)
-    # car = Car()
-    # assert car._odometer == 0, "Car does not set odometer correctly"
-
+    car = Car()
+    assert car._odometer == 0, "Car does not set odometer correctly"
+    test_car = Car(fuel=10)
+    assert test_car.fuel == 10, "Car does not set odometer correctly"
     # TODO: 2. write assert statements to show if Car sets the fuel correctly
     # Note that Car's __init__ function sets the fuel in one of two ways:
     # using the value passed in or the default
@@ -52,7 +53,7 @@ run_tests()
 
 # TODO: 3. Uncomment the following line and run the doctests
 # (PyCharm may see your >>> doctest comments and run doctests anyway.)
-# doctest.testmod()
+doctest.testmod()
 
 # TODO: 4. Fix the failing is_long_word function
 # (Don't change the tests, change the function!)
